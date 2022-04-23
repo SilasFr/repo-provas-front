4742b8a14f7b7511f3a4ec38e46055b9d8331f4e
SilasFr <silas23.fr@gmail.com>
2022-04-23 10:44:33 -0300

    feat: create feed dropdown

--- src/components/feedComponents.jsx
+++ src/components/feedComponents.jsx
@@ -1,17 +1,43 @@
 import styled from "styled-components";
-import { Container } from "@mui/material";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Container,
+} from "@mui/material";
+import { ExpandMoreRounded } from "@mui/icons-material";
+import { v4 as uuid } from "uuid";
+
+export default function SimpleAccordion(term) {
+  const [termTitle] = Object.keys(term);
+  const [subjects] = Object.values(term);
+  return (
+    <div key={uuid()}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreRounded />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <p>{termTitle}</p>
+        </AccordionSummary>
+        <AccordionDetails>
+          {subjects.map((subject) => {
+            return <p key={uuid()}>{subject}</p>;
+          })}
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
 
 const FeedContainer = styled(Container)`
   max-width: 700px;
   margin: 50px 0 0 0;
-  padding: 0 12px;
+  padding: 5px 12px;
 
   display: flex;
   flex-direction: column;
-
-  background-color: #fff;
-  box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3);
-  border-radius: 4px;
 `;
 
 export { FeedContainer };
--- src/pages/home/feed.jsx
+++ src/pages/home/feed.jsx
@@ -1,13 +1,44 @@
-import { FeedContainer } from "../../components/feedComponents";
+import SimpleAccordion, {
+  FeedContainer,
+} from "../../components/feedComponents";
 
 export function Feed() {
+  const example2 = [
+    { "10 Período": ["CSS", "HTML", "JavaScript"] },
+    { "20 Período": ["CSS3", "HTML5", "JavaScript mais poderoso"] },
+    { "30 Período": ["Sass", "React"] },
+    { "40 Período": ["CSS", "HTML", "JavaScript"] },
+    { "50 Período": ["CSS", "HTML", "JavaScript"] },
+    { "60 Período": ["CSS", "HTML", "JavaScript"] },
+  ];
   return (
     <FeedContainer>
-      <h1>Testing</h1>
-      <h1>Testing</h1>
-      <h1>Testing</h1>
-      <h1>Testing</h1>
-      <h1>Testing</h1>
+      {example2.map((term) => SimpleAccordion(term))}
     </FeedContainer>
   );
 }
+
+// const teste = (
+//   <FeedUl>
+//     {example2.map((term) => {
+//       const [termTitle] = Object.keys(term);
+//       const [subjects] = Object.values(term);
+
+//       return (
+//         <FeedItem key={uuid()}>
+//           <p>{termTitle}</p>
+//           <ul>
+//             {subjects.map((subject) => {
+//               console.log("matéria: ", subject);
+//               return (
+//                 <li>
+//                   <p>{subject}</p>
+//                 </li>
+//               );
+//             })}
+//           </ul>
+//         </FeedItem>
+//       );
+//     })}
+//   </FeedUl>
+// );
